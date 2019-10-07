433560b3f154e7afe6597232e5f914e5464a2821
YouHao <haoyou0807@163.com>
2019-10-07 23:39:15 +1000

    dynamic table

--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Card, Table } from 'antd';
+import axios from './../../axios/index';
+import Utils from './../../utils/utils';
 
 export default class BasicTable extends Component {
-  state = {};
+  state = { dataSource: [] };
   componentDidMount() {
     const dataSource = [
       {
@@ -39,13 +41,55 @@ export default class BasicTable extends Component {
     this.setState({
       dataSource
     });
+    this.request();
   }
+  request = () => {
+    let _this = this;
+    axios
+      .ajax({
+        url: '/table/list',
+        data: {
+          // params: {
+          //   page: this.params.page
+          // }
+        }
+      })
+      .then(res => {
+        if (res.code == 0) {
+          res.result.list.map((item, index) => {
+            item.key = index;
+          });
+          this.setState({
+            dataSource2: res.result.list,
+            selectedRowKeys: [],
+            selectedRows: null,
+            pagination: Utils.pagination(res, current => {
+              _this.params.page = current;
+              this.request();
+            })
+          });
+        }
+      });
+  };
   render() {
     const columns = [
       { title: 'id', dataIndex: 'id' },
       { title: 'UserName', dataIndex: 'userName' },
-      { title: 'Gender', dataIndex: 'sex' },
-      { title: 'State', dataIndex: 'state' },
+      {
+        title: 'Gender',
+        dataIndex: 'sex',
+        render(sex) {
+          return sex == 1 ? 'Male' : 'Female';
+        }
+      },
+      {
+        title: 'State',
+        dataIndex: 'state',
+        render(state) {
+          let config = { '1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E' };
+          return config[state];
+        }
+      },
       { title: 'Habit', dataIndex: 'habit' },
       { title: 'Birthday', dataIndex: 'birthday' },
       { title: 'Address', dataIndex: 'address' },
@@ -57,6 +101,14 @@ export default class BasicTable extends Component {
           <Table
             columns={columns}
             dataSource={this.state.dataSource}
+            bordered
+            pagination={false}
+          ></Table>
+        </Card>
+        <Card title='DynamicTable' style={{ margin: '10px 0' }}>
+          <Table
+            columns={columns}
+            dataSource={this.state.dataSource2}
             bordered
             pagination={false}
           ></Table>
